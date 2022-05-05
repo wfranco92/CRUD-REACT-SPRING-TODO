03ae75ebfa35c6f81309c21538c889f674b91d5d
Willian Franco <ing.wfranco@gmail.com>
2022-05-05 11:09:05 -0500

    feat - add backend - se crea proyecto backend con spring boot, model, service, repo, control

---
+++ backend/src/main/java/com/sofka/williamfranco/backend/Service/TodoService.java
@@ -0,0 +1,26 @@
+package com.sofka.williamfranco.backend.Service;
+
+import com.sofka.williamfranco.backend.Model.Todo;
+import com.sofka.williamfranco.backend.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public Iterable<Todo> list(){
+        return todoRepository.findAll();
+    }
+
+    public Todo save(Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    public void delete(Long id){
+        todoRepository.delete(get(id));
+    }
+
+    public Todo get(Long id){
+        return todoRepository.findById(id).orElseThrow();
+    }
+}
